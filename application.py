e3e3353dfa62d2261b78cd8570678b4e94859013
Realsponge <haomin@hotmail.de>
2019-02-05 20:23:49 +0000

    style aus dem html nehmen in eine externe css datei

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+
 conn = sqlite3.connect("info.db", check_same_thread=False)
 
 
@@ -65,9 +65,9 @@ def suggestion():
         convxml()
 
 
-        return render_template("marker.html")
+        return render_template("suggestion.html")
     else:
-        return render_template("marker.html")
+        return render_template("suggestion.html")
 
 @app.route("/japan")
 def japan():
